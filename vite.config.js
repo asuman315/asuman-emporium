61cf08d9afdf8392a4c828a01ccf1b27f5c71ae1
asumanwebdev <asumanwebdev@gmail.com>
2022-06-09 02:46:13 +0300

    remove netlify config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import shopifyConfig from './shopify.config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [hydrogen(shopifyConfig), netlifyPlugin()],
+  plugins: [hydrogen(shopifyConfig)],
   optimizeDeps: {include: ['@headlessui/react']},
   test: {
     globals: true,
